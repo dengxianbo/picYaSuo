dc5d1307295735fbf87dca87637c8ff66b35a36e
QingkuangAbo <136253075@qq.com>
2019-04-17 14:44:34 +0800

    first commit

---
+++ MyYa-master/app/src/main/java/com/example/administrator/myya_master/MainActivity.java
@@ -0,0 +1,251 @@
+package com.example.administrator.myya_master;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import me.iwf.photopicker.PhotoPicker;
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.functions.Func1;
+import rx.schedulers.Schedulers;
+import top.zibin.luban.Luban;
+import top.zibin.luban.OnCompressListener;
+
+import static android.R.attr.path;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView fileSize;
+    private TextView imageSize;
+    private TextView thumbFileSize;
+    private TextView thumbImageSize;
+    private ImageView image;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        fileSize = (TextView) findViewById(R.id.file_size);
+        imageSize = (TextView) findViewById(R.id.image_size);
+        thumbFileSize = (TextView) findViewById(R.id.thumb_file_size);
+        thumbImageSize = (TextView) findViewById(R.id.thumb_image_size);
+        image = (ImageView) findViewById(R.id.image);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PhotoPicker.builder()
+                        .setPhotoCount(5)
+                        .setShowCamera(true)
+                        .setShowGif(true)
+                        .setPreviewEnabled(false)
+                        .start(MainActivity.this, PhotoPicker.REQUEST_CODE);
+
+            }
+        });
+    }
+
+    /**
+     * 压缩单张图片 Listener 方式
+     */
+    String filePath;
+    private void compressWithLs(File file) {
+        Luban.get(this)
+                .load(file)
+                .putGear(Luban.THIRD_GEAR)
+                .setFilename(System.currentTimeMillis() + "")
+                .setCompressListener(new OnCompressListener() {
+                    @Override
+                    public void onStart() {
+
+                        Toast.makeText(MainActivity.this, "I'm start", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onSuccess(File file) {
+                        Log.i("path", file.getAbsolutePath());
+                        Glide.with(MainActivity.this).load(file).into(image);
+
+                        filePath=file.getAbsolutePath();
+                        Bitmap bitmap = compressBySize(filePath,Luban.get(getApplicationContext()).getImageSize(file.getPath())[0], Luban.get(getApplicationContext()).getImageSize(file.getPath())[1]);  //设置压缩后图片的高度和宽度
+                        Log.e("TAG", "bitmap" + bitmap);
+                        saveImage(bitmap);
+//                        saveImage(file.);
+                        Log.e("TAG","----eeeee----"+file.getAbsolutePath());
+
+                        thumbFileSize.setText(file.length() / 1024 + "k");
+                        thumbImageSize.setText(Luban.get(getApplicationContext()).getImageSize(file.getPath())[0] + " * " + Luban.get(getApplicationContext()).getImageSize(file.getPath())[1]);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+                }).launch();
+    }
+    // 压缩图片尺寸
+    private static Bitmap compressBySize(String pathName, int targetWidth,
+                                         int targetHeight) {
+        BitmapFactory.Options opts = new BitmapFactory.Options();
+        opts.inJustDecodeBounds = true;// 不去真的解析图片，只是获取图片的头部信息，包含宽高等；
+        Bitmap bitmap = BitmapFactory.decodeFile(pathName, opts);
+        // 得到图片的宽度、高度；
+        float imgWidth = opts.outWidth;
+        float imgHeight = opts.outHeight;
+        // 分别计算图片宽度、高度与目标宽度、高度的比例；取大于等于该比例的最小整数；
+        int widthRatio = (int) Math.ceil(imgWidth / (float) targetWidth);
+        int heightRatio = (int) Math.ceil(imgHeight / (float) targetHeight);
+        opts.inSampleSize = 1;
+        if (widthRatio > 1 || widthRatio > 1) {
+            if (widthRatio > heightRatio) {
+                opts.inSampleSize = widthRatio;
+            } else {
+                opts.inSampleSize = heightRatio;
+            }
+        }
+        // 设置好缩放比例后，加载图片进内容；
+        opts.inJustDecodeBounds = false;
+        bitmap = BitmapFactory.decodeFile(pathName, opts);
+        return bitmap;
+    }
+    public static void saveImage(Bitmap bmp) {
+        File appDir = new File(Environment.getExternalStorageDirectory(), "Yasuo");
+        if (!appDir.exists()) {
+            appDir.mkdir();
+        }
+        String fileName = System.currentTimeMillis() + ".jpg";
+        File file = new File(appDir, fileName);
+        try {
+            FileOutputStream fos = new FileOutputStream(file);
+            bmp.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+            fos.flush();
+            fos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public static void saveImageToGallery(Context context, Bitmap bmp) {
+        // 首先保存图片
+        File appDir = new File(Environment.getExternalStorageDirectory(), "Yasuo");
+        if (!appDir.exists()) {
+            appDir.mkdir();
+        }
+        String fileName = System.currentTimeMillis() + ".jpg";
+        File file = new File(appDir, fileName);
+        try {
+            FileOutputStream fos = new FileOutputStream(file);
+            bmp.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+            fos.flush();
+            fos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // 其次把文件插入到系统图库
+        try {
+            MediaStore.Images.Media.insertImage(context.getContentResolver(),
+                    file.getAbsolutePath(), fileName, null);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        // 最后通知图库更新
+        context.sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.parse("file://" + path)));
+    }
+    /**
+     * 压缩单张图片 RxJava 方式
+     */
+    private void compressWithRx(File file) {
+        Luban.get(this)
+                .load(file)
+                .putGear(Luban.THIRD_GEAR)
+                .asObservable()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnError(new Action1<Throwable>() {
+                    @Override
+                    public void call(Throwable throwable) {
+                        throwable.printStackTrace();
+                    }
+                })
+                .onErrorResumeNext(new Func1<Throwable, Observable<? extends File>>() {
+                    @Override
+                    public Observable<? extends File> call(Throwable throwable) {
+                        return Observable.empty();
+                    }
+                })
+                .subscribe(new Action1<File>() {
+                    @Override
+                    public void call(File file) {
+                        Glide.with(MainActivity.this).load(file).into(image);
+
+                        Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+                        Uri uri = Uri.fromFile(file);
+                        intent.setData(uri);
+
+                        Log.e("TAG","-----eeeee----"+uri);
+                        MainActivity.this.sendBroadcast(intent);
+
+                        thumbFileSize.setText(file.length() / 1024 + "k");
+                        thumbImageSize.setText(Luban.get(getApplicationContext()).getImageSize(file.getPath())[0] + " * " + Luban.get(getApplicationContext()).getImageSize(file.getPath())[1]);
+                    }
+                });
+    }
+
+
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK && requestCode == PhotoPicker.REQUEST_CODE) {
+            if (data != null) {
+                ArrayList<String> photos = data.getStringArrayListExtra(PhotoPicker.KEY_SELECTED_PHOTOS);
+
+                File imgFile = new File(photos.get(0));
+                fileSize.setText(imgFile.length() / 1024 + "k");
+                imageSize.setText(Luban.get(this).getImageSize(imgFile.getPath())[0] + " * " + Luban.get(this).getImageSize(imgFile.getPath())[1]);
+
+                for (int i = 0; i < photos.size(); i++)
+                    compressWithLs(new File(photos.get(i))
+//                    compressWithRx(new File(photos.get(i))
+                );
+            }
+        }
+    }
+
+}
